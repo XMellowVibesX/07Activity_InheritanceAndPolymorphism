eff09bfd44f2e1b406839b1dfa682b765ca987f6
Casabar, K - Mell <xmellowvibesx@gmail.com>
2024-11-05 03:10:16 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using UserNamespace;
+
+namespace Program
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Administrator admin = new Administrator("admin_JD001", "AJD-001", "Jane Doe");
+
+            Console.WriteLine("""
+                Logging in...
+                Name: Jane Doe
+                Password: AJD-001
+                ID: admin_JD001
+                Please wait...
+
+                """);
+            
+            if (admin.verifyLogin("admin_JD001", "AJD-001"))
+            {
+                Console.WriteLine("""
+                    =====================================
+
+                    Login Successful! Welcome Jane Doe!
+                    
+                    =====================================
+                    """);
+            } else { Console.WriteLine("Login failed! Try Again!"); }
+
+            Console.WriteLine("""
+
+                Updating Username and Password...
+                New username: Janie123
+                New password: JD123
+                Please wait...
+                """);
+
+            admin.updateAdminName("Janie123");
+            admin.updatePassword("JD123");
+
+            Console.WriteLine("\nUpdate successful! Please check to see if its correct:");
+            Console.WriteLine("New name: " + admin.getUsername());
+            Console.WriteLine("New password: " + admin.getPassword());
+        }
+    }
+    
+}
---
+++ UserNamespace.cs
@@ -0,0 +1,52 @@
+﻿namespace UserNamespace
+{
+    public abstract class User
+    {
+        protected string userID;
+        protected string userPassword;
+
+        public User(string userID, string userPassword)
+        {
+            this.userID = userID;
+            this.userPassword = userPassword;
+        }
+
+        public bool verifyLogin(string id, string pass)
+        {
+            return this.userID == id && this.userPassword == pass;
+        }
+
+        public abstract void updatePassword(string newPass);
+
+    }
+
+    public class Administrator : User
+    {
+        private string adminName;
+
+        public Administrator(string userID, string userPassword, string adminName) : base(userID, userPassword)
+        {
+            this.adminName = adminName;
+        }
+
+        public void updateAdminName(string newName)
+        {
+            this.adminName = newName;
+        }
+
+        public override void updatePassword(string newPass)
+        {
+            this.userPassword = newPass;
+        }
+
+        public string getUsername()
+        {
+            return this.adminName;
+        }
+
+        public string getPassword()
+        {
+            return this.userPassword;
+        }
+    }
+}
